c634733e3b9945c9ab7aa566ab2f70b4bfee3efa
chenjixiv587 <darlinglin046@gmail.com>
2024-01-03 21:18:09 +0800

    make a progress

--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
 # Ask for user name 
-s_name = input("What is your name? \n")
+name:str = input("What is your name? \n").strip().title()
 
 # echo the name
-
-print("hello, " + s_name, sep="", end="")
+l_name:list = name.split(" ")
+first_name, last_name = l_name
+# print("hello, " + s_name, sep="", end="")
+print(f"hello {name}")
+print(l_name)
+print(first_name, last_name)
+# learn the things 
---
+++ hello2.py
@@ -0,0 +1,4 @@
+num1:float = float(input("What's x? "))
+num2:float = float(input("What's y? "))
+num3 = round(num1 + num2, 4)
+print(f"{num3:,}")
